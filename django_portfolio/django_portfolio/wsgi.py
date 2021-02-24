8193337450782485e96e5aa87ef63c4fd97272e2
Japi MacKorkee <janne.korkee@gmail.com>
2021-02-24 07:24:26 +0200

    added some buttons for managing posts, sqreen security initial settings and gitignore updated.

--- django_portfolio/django_portfolio/wsgi.py
+++ django_portfolio/django_portfolio/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+#sqreen
+#import sqreen
+#sqreen.start()
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio.settings')
